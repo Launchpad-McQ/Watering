a3ca3164d03c30c055e0631e65786b182a9dbcca
Launchpad-McQ <magnus.k.wiberg@gmail.com>
2018-01-06 03:26:33 +0100

    Working interface with relays and feedback

--- relay.py
+++ relay.py
@@ -5,23 +5,15 @@ import time
 import os
 import RPi.GPIO as GPIO
 # from relaytest import setpins, relayon
+import config
 
+print(config.status)
+pinlist=[2, 3, 4, 17, 27, 22, 5, 6, 13, 19, 26, 14, 15, 18, 23, 24, 25, 7, 12, 16, 20, 21]
+relaycount = 16
 
 # For printing to system console. Useful when using multiple threads.
 def consoleprint(string):
     os.system("syslog -s -l error \" something " + str(string) + "\"")
-
-
-"""
-print(mcp3008.readadc(1))
-setpins()
-relayon(9,0.3)
-time.sleep(0.3)
-relayon(9,0.3)
-time.sleep(0.3)
-relayon(9,0.3)
-time.sleep(0.3)
-"""
 
 
 # Relay class. Should be started in thread in relayControll.
@@ -33,89 +25,71 @@ class Relay:
     def __init__(self, pin=None):
         self.pin = pin
 
-    # Turn on relay#
-    def turn(self, on):
+    # Turn on relay turn(True) = turn on, turn(False) turn off.
+    def turn(self, on, relaynum, duration = 3):
         if self.on != on is True:   # Turning relay on.
             GPIO.output(self.pin, GPIO.LOW)
             self.on = on
+            config.status["relayon"][relaynum] = self.on
+            self.loop(duration, relaynum)
             return
         if self.on != on is False:  # Turning relay off.
             GPIO.output(self.pin, GPIO.HIGH)
             self.on = on
+            config.status["relayon"][relaynum] = self.on
             return
     # Loop running untill turned off from other thread.
 
-    def loop(self):
-        print("in loop")
-        while True:
+    def loop(self,duration, relaynum):
+        for i in range(1,duration*10):
             if self.on is False:
                 GPIO.output(self.pin, GPIO.HIGH)
-                consoleprint(str(self.pin) + " Im out")
+                config.status["relayon"][relaynum] = self.on
                 break
-            consoleprint(str(self.pin) + " im on")
             time.sleep(0.1)
-
+        self.turn(False, relaynum)
 
 class RelayControll:
+    global relaycount
+    global pinlist
     relays = []
     thread = None
-    relaycount=16
-    pinlist=[2, 3, 4, 17, 27, 22, 5, 6, 13, 19, 26, 14, 15, 18, 23, 24, 25, 7, 12, 16, 20, 21],
 
-    def __init__(self, pinlist=pinlist, relaycount=relaycount):  # BCM pins. Excluding pins used for SPI0.
-        self.pinlist = pinlist[0: relaycount]
+    def __init__(self,pinlist=pinlist, relaycount=relaycount):  # BCM pins. Excluding pins used for SPI0.
+        self.relaycount = relaycount
+        self.pinlist = pinlist[0: self.relaycount]
         for pin in pinlist:
             self.relays.append(Relay(pin))  # Adding relays to a list.
         self.setpins(pinlist)
 
-    def turnOnRelay(self, relaynum):
-        self.relays[relaynum].turn(True)
+    def turnOnRelay(self, relaynum, duration = 2):
+        self.thread= threading.Thread(target=self.relays[relaynum].turn, args=(True, relaynum, duration))
+        self.thread.start()
 
-    def turnOffRelay(self, relaynum):
-        self.relays[relaynum].turn(False)
+    def turnOffRelay(self, relaynum, duration = 2):
+        self.thread= threading.Thread(target=self.relays[relaynum].turn, args=(False, relaynum, duration))
+        self.thread.start()
+        time.sleep(0.3)
 
-    def setpins(self, pinist):
+    def setpins(self, pinlist):
         GPIO.setmode(GPIO.BCM)
         # loop through pins and set mode and state to 'low'
         for i in self.pinlist:
             GPIO.setup(i, GPIO.OUT)
             GPIO.output(i, GPIO.HIGH)
 
-    # Relay on for desired duration (delay).
-    def ontimer(self, relaynum, delay):
-        self.relays[relaynum].turn(True)
-        consoleprint("thread relay on")
-        time.sleep(delay)
-        self.relays[relaynum].turn(False)
-        consoleprint("thread relay off")
-
-    # Turning on relay in new thread.
-    def onbutton(self, relaynum, delay):
-        self.thread = threading.Thread(target=self.ontimer, args=(relaynum,
-                                                                  delay, ))
-        self.thread.start()
 
     # Reset GPIO settings.
     def cleanup(self):
         GPIO.cleanup()
 
-controll = RelayControll()
-controll.onbutton(0, 3)
-controll.turnOffRelay(0)
-time.sleep(1)
-controll.turnOnRelay(0)
-time.sleep(10)
-controll.cleanup()
-
 """
-relay1 = Relay(0,)
-relay2 = Relay(0,)
-t1 = threading.Thread(target=relay1.turn, args=(True,))
-t1.start()
-time.sleep(1)
-t1 = threading.Thread(target=relay1.turn, args=(False,))
-print(threading.activeCount())
-t1.start()
-print("slutet")
-
+#for test
+controll = RelayControll()
+controll.turnOnRelay(0,5)
+time.sleep(2)
+controll.turnOffRelay(0)
+controll.turnOnRelay(0,5)
+time.sleep(2)
+controll.cleanup()
 """
--- webtest.py
+++ webtest.py
@@ -1,51 +1,63 @@
 import asyncio
 import websockets
 import json
+import relay
+import config
 
 relaycount = 16
 sensorcount = 16
+pinlist=[2, 3, 4, 17, 27, 22, 5, 6, 13, 19, 26, 14, 15, 18, 23, 24, 25, 7, 12, 16, 20, 21]
 
-relayon=[False] * relaycount
-sensorval= [0] * sensorcount
+relayon = [False] * relaycount
+sensorval = [0] * sensorcount
 
-status={"relayon":relayon, "sensorval":sensorval}
+#status = {"relayon":relayon, "sensorval":sensorval}
+
+controller = relay.RelayControll(pinlist, relaycount)
+
+# controller.turnOnRelay(relaynum,duration)
+"""time.sleep(2)
+controller.turnOffRelay(0)
+controller.turnOnRelay(0,5)
+time.sleep(2)
+controller.cleanup()"""
+
+try:
+    async def handler(websocket, path):
+        consumer_task = asyncio.ensure_future(consumer_handler(websocket))
+        producer_task = asyncio.ensure_future(producer_handler(websocket))
+        done, pending = await asyncio.wait(
+            [consumer_task, producer_task],
+            return_when=asyncio.FIRST_COMPLETED,
+        )
+
+        for task in pending:
+            task.cancel()
+
+    async def consumer_handler(websocket):
+        async for message in websocket:
+            await consumer(message)
+
+    async def producer_handler(websocket):
+        while True:
+            message = await producer()
+            await websocket.send(message)
+            await asyncio.sleep(0.1)
+
+    async def consumer(message):
+        relaynum = int(message)
+        controller.turnOnRelay(relaynum,2)
+
+    async def producer():
+        message = json.dumps(config.status)
+        return message
+
+    start_server = websockets.serve(handler, '127.0.0.1', 9998)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
 
 
 
-
-async def handler(websocket, path):
-    consumer_task = asyncio.ensure_future(consumer_handler(websocket))
-    producer_task = asyncio.ensure_future(producer_handler(websocket))
-    done, pending = await asyncio.wait(
-        [consumer_task, producer_task],
-        return_when=asyncio.FIRST_COMPLETED,
-    )
-
-    for task in pending:
-        task.cancel()
-
-async def consumer_handler(websocket):
-    async for message in websocket:
-        await consumer(message)
-
-async def producer_handler(websocket):
-    while True:
-        message = await producer()
-        await websocket.send(message)
-        await asyncio.sleep(0.1)
-
-async def consumer(message):
-    global status
-    status["relayon"][int(message)] = not status["relayon"][int(message)]
-    print(message)
-
-async def producer():
-    global status
-    message = json.dumps(status)
-#    now = datetime.datetime.utcnow().isoformat() + 'Z'
-    return message
-
-start_server = websockets.serve(handler, '127.0.0.1', 9998)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+except KeyboardInterrupt:
+    controller.cleanup()
